b76096c01f14f79ccea34ea2674a102c75161063
Gabriel Baú <gbauher22@gmail.com>
2025-06-11 12:34:55 -0300

    Architecture and Main structure

---
+++ src/exercicio1.rs
@@ -0,0 +1,4 @@
+pub fn executar() {
+    println!("Executando Exercício 1: Gerar combinações de S15, S14, S13, S12 e S11");
+    // Aqui virá sua lógica usando itertools + Vec<Vec<u8>>
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
+mod common;
+mod exercicio1;
+mod exercicio2;
+mod exercicio3;
+mod exercicio4;
+mod exercicio5;
+mod exercicio6;
+mod exercicio7;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Escolha o exercício (1 a 7):");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    match input.trim() {
+        "1" => exercicio1::executar(),
+        _ => println!("Exercício inválido"),
+    }
 }
