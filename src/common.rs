e14bbb97a5dc67144f5ee01eb2cf9217b4645080
Matheus Girardi <matheus.girardi@brf.com>
2025-06-17 18:31:38 +0000

    chore

--- src/common.rs
+++ src/common.rs
@@ -62,7 +62,7 @@ pub fn limpar_output() -> io::Result<()> {
         let entry = entry?;
         let path = entry.path();
         if let Some(name) = path.file_name().and_then(|s| s.to_str()) {
-            if name == ".gitkeep" {
+            if name == ".gitkeep" | name == "combinacoes"{
                 continue;
             }
         }
